935afcef6b155f2289b4cb8274069e078ae8b97d
MJ159023 <michael.bruneau@stu.ocsb.ca>
2026-01-08 04:55:19 +0000

    save

--- run_linter.sh
+++ run_linter.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-java -jar ./linter/checkstyle-11.1.0-all.app -c ./linter/sun_checks.xml *.java
+java -jar ./linter/checkstyle-8.45.1-all.jar \
+     -c ./linter/sun_checks.xml \
+     *.java
